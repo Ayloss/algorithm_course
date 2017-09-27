fd87c439b3a6813b55fb544582b0af7db9647269
StatusCode200 <358941501@qq.com>
2017-09-28 01:21:26 +0800

    lab1 q1 method2 explain.

--- src/lab1/q1_1/method2/Main.java
+++ src/lab1/q1_1/method2/Main.java
@@ -3,12 +3,19 @@ package lab1.q1_1.method2;
 import java.util.Scanner;
 
 /**
+ * 思路：最高位出现次数容易看出规律，去掉最高位后所得数字+1即为最高位出现的次数。比如2999中,最高位2的出现次数为999 + 1 = 1000次
+ * 比如对于三位数abc,可以先计算
+ * abc --- a00。
+ * 然后计算(a-1)99 --- (a-1)00,(a-2)99 -- (a-2)00 ,,,,,,
+ * 可以发现这几个的计算方法都是一致的，先算最高位，再算去掉最高位的情况。
+ *
  * Created by status200 on 2017/9/27.
  */
 public class Main {
 
     private static int[] numberAppearanceTimes;
 
+    // 初始化数据
     public static void init() {
         numberAppearanceTimes = new int[10];
         for (int i = 0; i < 10; i++) {
@@ -17,6 +24,12 @@ public class Main {
     }
 
 
+    /**
+     * 根据开始时的位数、当前的位数进行计算
+     * @param num 数字
+     * @param beginLevel 代表开始时的位数,为10的倍数
+     * @param currentLevel 代表当前的位数,为10的倍数
+     */
     public static void count(int num, int beginLevel, int currentLevel) {
 
         // 只有1位的情况
@@ -27,24 +40,36 @@ public class Main {
             return;
         }
 
+        // 首位数字
         int firstNumber = num / currentLevel;
+        // 去除首位数字的部分
         int numExceptFirstPart = num % currentLevel;
+
+        // 出现10023这样的情况
+        // 去除首位后,结果为0023
         if (num < currentLevel) {
             numberAppearanceTimes[0] += (num + 1);
             count(num, beginLevel, currentLevel / 10);
 
-        } else {
+        }
+        // 计算首位数字,并依次减一进行计算
+        else {
+            // 首位数字
             numberAppearanceTimes[firstNumber] += (numExceptFirstPart + 1);
             count(numExceptFirstPart, beginLevel, currentLevel / 10);
 
+            // 决定开始循环的数
+            // 对于比如1237这样的情况,计算完最高位1,计算237时,应当把0的次数考虑进去。比如页码为1037
             int beginNum = beginLevel > currentLevel ? 0 : 1;
             for (int i = beginNum; i < firstNumber; i++) {
                 numberAppearanceTimes[i] += currentLevel;
+
                 if(i != 0) {
                     count(currentLevel - 1, beginLevel, currentLevel / 10);
                 }
             }
 
+            // 以当前的位数-1作为开始的位数,计算减去一位的结果
             count(currentLevel - 1, beginLevel / 10, currentLevel / 10);
         }
     }
