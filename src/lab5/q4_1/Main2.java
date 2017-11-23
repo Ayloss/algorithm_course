fc2bb695ad735649ec4a85486c26dd09080ea07c
status200 <358941501@qq.com>
2017-11-23 19:00:51 +0800

    lab5.

--- src/lab5/q4_1/Main2.java
+++ src/lab5/q4_1/Main2.java
@@ -5,12 +5,7 @@ import java.util.Scanner;
 /**
  * Created by status200 on 2017/11/22.
  */
-public class Main {
-
-    // 无向图，代表每个活动和其他活动的相容性
-    // 0代表连通，即相容
-    // 1代表不连通，即不相容
-    public static int G[][];
+public class Main2 {
 
     // 每个节点使用的颜色
     public static int C[];
@@ -21,38 +16,18 @@ public class Main {
     // 使用的颜色种数
     public static int colorNum;
 
-    // 输入转化为图
-    public static void initGraph(int startTime[], int endTime[], int n) {
-        G = new int[n][n];
-        colorStack = new int[1000];
-        C = new int[n];
+    // 活动开始时间
+    public static int startTime[];
 
-        for (int i = 0; i < n; i++) {
-            // 颜色设为0,0代表未访问
-            C[i] = 0;
-
-            int start = startTime[i];
-            int end = endTime[i];
-
-            for (int j = 0; j < n; j++) {
-
-                if(i == j) {
-                    G[i][j] = 0;
-                } else {
-                    // 开始时间或者截止时间处在当前活动的时间范围内
-                    if((start >= startTime[j] && start <= endTime[j])
-                            || (end >= startTime[j] && end <= endTime[j])) {
-                        G[i][j] = G[j][i] = 1;
-                    } else {
-                        G[i][j] = G[j][i] = 0;
-                    }
-                }
-            }
-        }
-    }
+    // 活动结束时间
+    public static int endTime[];
 
     public static boolean isConnected(int i,int j) {
-        return G[i][j] == 1;
+        int start = startTime[i];
+        int end = endTime[i];
+
+        return (start >= startTime[j] && start <= endTime[j])
+                || (end >= startTime[j] && end <= endTime[j]);
     }
 
     public static boolean isVisited(int i) {
@@ -89,6 +64,7 @@ public class Main {
         colorStack[0] = 1;
         colorNum = 1;
 
+
         for (int i = 1; i < n; i++) {
 
             // 表示每种颜色是否可用
@@ -124,15 +100,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        int[] startTime = new int[n];
-        int[] endTime = new int[n];
+        startTime = new int[n];
+        endTime = new int[n];
 
         for (int i = 0; i < n; i++) {
             startTime[i] = scanner.nextInt();
             endTime[i] = scanner.nextInt();
         }
 
-        initGraph(startTime, endTime, n);
         System.out.println(mColor(n));
     }
 }
